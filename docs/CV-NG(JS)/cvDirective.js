bece117d815a61c23d8844d233e6c8cb6c021373
Jay <spamjaydo@gmail.com>
2019-08-06 23:45:44 +0300

    ng js update

--- docs/CV-NG(JS)/cvDirective.js
+++ docs/CV-NG(JS)/cvDirective.js
@@ -1,9 +1,6 @@
 angular.module('cvngjs')
-
     .directive('cvJobsDrctv', cvJobsDrctv);
-
     cvJobsDrctv.$inject = ['$compile'];
-
     function cvJobsDrctv($compile) {
         return {
                 restrict: 'EAC',
@@ -11,15 +8,60 @@ angular.module('cvngjs')
                 replace: true,
                 link: function(scope, element, attrs){
                     console.log( "cvJobs directive 'link' enter" ) ;
-
-                    scope.selectedJob = 0;
-                    scope.activeJob = scope.cvAll.jobs[ scope.selectedJob ]; 
-				
 				    function CvItemClick(d,i,e){
 					    var ClickedItem = d ;
                         console.log( "cvJobs-CvItemClick ClickedSegment: ", ClickedItem );
 					    scope.cvItemclick( ClickedItem ) ;
 				    }
-	     	    }
+	            }
 	    }
     }
+
+angular.module('cvngjs')
+    .directive('cvLingosDrctv', cvLingosDrctv);
+    cvLingosDrctv.$inject = ['$compile'];
+    function cvLingosDrctv($compile) {
+        return {
+                restrict: 'EAC',
+                template: '<div><h3 ng-repeat="lingo in cvAll.languages">{{ lingo }}</h3></div>',
+                replace: true,
+                link: function(scope, element, attrs){
+                    console.log( "cvLingo directive 'link' enter" ) ;
+                }
+        }
+    }
+
+    angular.module('cvngjs')
+    .directive('cvDomainsDrctv', cvDomainsDrctv);
+    cvDomainsDrctv.$inject = ['$compile'];
+    function cvDomainsDrctv($compile) {
+        return {
+                restrict: 'EAC',
+                template: '<h3 ng-repeat="domain in domains">{{ domain.domain }} <span>{{ domain.experience }} (yrs)</span></h3>',
+                replace: true,
+                link: function(scope, element, attrs){
+                    console.log( "cvDomain directive 'link' enter" ) ;
+                    scope.domains = [{ "domain": 'jobdom', "experience": 0 }];
+                    if( scope.cvAll.jobs ){  
+                        scope.domains = [];
+                        let jobdom = "";
+                        let domainExperience = 0;
+
+                        for(let i=0; i<scope.cvAll.jobDomains.length; i++){
+                            jobdom = scope.cvAll.jobDomains[i];
+                            console.log("jobdom each:", jobdom )
+                            domainExperience = 0;
+
+                                for(let j=0; j < scope.cvAll.jobs.length; j++){
+                                    let jobexp = scope.cvAll.jobs[j].durationMonths;
+                                    if( scope.cvAll.jobs[j].jobDomain.includes(jobdom) ){
+                                        domainExperience += jobexp;
+                                    }
+                                }
+                            let domainObj = { "domain": (jobdom==="CS")? "Customer Service":jobdom, "experience": ( domainExperience / 12).toFixed(0) }
+                            scope.domains.push( domainObj );
+                        }
+                    }
+                }
+        }
+    }    
--- docs/CV-NG(JS)/cvNG.js
+++ docs/CV-NG(JS)/cvNG.js
@@ -7,5 +7,20 @@ angular.module('cvngjs')
 
         function cvInit( $rootScope, $scope, $http, cvService, cvFactory) { 
         	$scope 	= $rootScope;
-        	console.log("enter cvInit: arrays loaded from js:", (cv.jobs.length>0) );
+            console.log("enter cvInit: arrays loaded from js:", (cv.jobs.length>0) );
+            
+            $scope.activeTab    =   0;
+            $scope.isActTab     =   (tabNr)=>{ return (tabNr === $scope.activeTab);  }
+            $scope.tabHandler   =   (tabNr)=>{  $scope.activeTab = tabNr 
+                                                console.log("active tab = ", tabNr );    
+                                    }
+
+            $scope.activeJob    =   -1;
+            $scope.isActJob     =   (jobNr)=>{ return (jobNr === $scope.activeJob);  }
+            $scope.jobHandler   =   (jobNr)=>{  // if job is expanded, collapse
+                                                (jobNr===$scope.activeJob)? (jobNr = -1):(jobNr=jobNr)
+                                                $scope.activeJob = jobNr ;
+                                                console.log("active job = ", jobNr );    
+                                    }
+
         }
