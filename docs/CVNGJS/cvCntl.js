7e377a1023c30b339403c18f7f1556fe72bf8249
Jay <spamjaydo@gmail.com>
2019-08-12 22:05:41 +0300

    cntrl

--- docs/CVNGJS/cvCntl.js
+++ docs/CVNGJS/cvCntl.js
@@ -32,33 +32,39 @@ app.factory(
 app.controller('cvJSONCntrl', cvJSONCntrl );
 cvJSONCntrl.$inject = ['$scope', '$rootScope', '$http', '$filter' ,'cvJSONFctr'];
 function cvJSONCntrl($scope, $rootScope, $http, $filter ,cvJSONFctr) {
-	$scope = $rootScope.CVcntrl ;
-    //	console.log('valBOS: ' + valBOS  +  ' | valBOL: ' + valBOL );
-    $scope.CVcntrl.allData = [];
-    $scope.CVcntrl.getCVAll =   ()=>{ 
-                                        cvJSONFctr.getAllCVData().then(
-                                            (data)=>{
-                                                $scope.CVcntrl.allData = data;
-                                                console.log(
-                                                    "controller log, getCVSection after : ", lr,
-                                                    "scope.CVcntrl.allData = data", $scope.CVcntrl.allData 
-                                                )
-                                            }
-                                        )
-                                    }
-                                    
-    $scope.CVcntrl.someData = [];
-    $scope.CVcntrl.getCVSection = (section)=>{
-            cvJSONFctr.getSomeCVData(section).then(
-                (data)=>{
-                    $scope.CVcntrl.someData = data[section];
-                    console.log(
-                        "controller log, getCVSection after : ", lr,
-                        "scope.CVcntrl.someData = data[section]", $scope.CVcntrl.someData 
-                    )
-                }
-            )
+    $scope = $rootScope;
+
+    if( !$scope.CVcntrl ){
+        $scope = $rootScope.CVcntrl ;
+        //	console.log('valBOS: ' + valBOS  +  ' | valBOL: ' + valBOL );
+        $scope.CVcntrl.allData = [];
+        $scope.CVcntrl.getCVAll =   ()=>{ 
+                                            cvJSONFctr.getAllCVData().then(
+                                                (data)=>{
+                                                    $scope.CVcntrl.allData = data;
+                                                    console.log(
+                                                        "controller log, getCVSection after : ", lr,
+                                                        "scope.CVcntrl.allData = data", $scope.CVcntrl.allData 
+                                                    )
+                                                }
+                                            )
         }
- console.log("exit cvJSONCntrl") ;
+                                        
+        $scope.CVcntrl.someData = [];
+        $scope.CVcntrl.getCVSection = (section)=>{
+                cvJSONFctr.getSomeCVData(section).then(
+                    (data)=>{
+                        $scope.CVcntrl.someData = data[section];
+                        console.log(
+                            "controller log, getCVSection after : ", lr,
+                            "scope.CVcntrl.someData = data[section]", $scope.CVcntrl.someData 
+                        )
+                    }
+                )
+        
+        }
+    }
+ 
+    console.log("exit cvJSONCntrl") ;
 }
 
